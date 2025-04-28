a089ffe5d2fbcec8cc7e49a39ce41133324ba864
admin <didorenko1305@gmail.com>
2025-04-29 00:41:59 +0200

    fix index.js and vite.config.ts

--- backend/index.js
+++ backend/index.js
@@ -119,10 +119,9 @@ app.post('/sort', (req, res) => {
 
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.get('*', (req, res) => {
+app.get('', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
 });
-
 // ============ Запуск сервера ============
 
 app.listen(PORT, () => {
