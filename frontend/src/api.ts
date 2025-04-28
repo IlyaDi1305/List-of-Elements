ef28393b868996ab5744e18196aa1613a9c8b153
admin <didorenko1305@gmail.com>
2025-04-29 01:04:50 +0200

    fix api

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3001';
+const API_URL = '';
 
 export const getItems = async (search = '', offset = 0, limit = 20) => {
     const response = await axios.get(`${API_URL}/items`, {
